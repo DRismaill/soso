f6e837cde2d821d63880bfe265ab07734f0bf66a
Ismail Ameksa <117262493+DRismaill@users.noreply.github.com>
2023-09-14 00:39:04 +0200

    aanpassingen header

--- js/script.js
+++ js/script.js
@@ -63,8 +63,62 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+// Voeg een click-eventlistener toe aan de dropdown-knop
+document.getElementById("dropdown-button-id").addEventListener("click", function () {
+    // Haal een referentie naar het dropdown-menu-element op
+    const dropdownMenu = document.getElementById("dropdown-menu");
 
+    // Toggle de "active" klasse om het menu te tonen/verbergen
+    dropdownMenu.classList.toggle("active");
 
+    // Als het menu actief is, start de animatie voor elk <li> element
+    if (dropdownMenu.classList.contains("active")) {
+        animateDropdownItems();
+    } else {
+        // Als het menu niet actief is, voer de omgekeerde animatie uit
+        reverseAnimateDropdownItems();
+    }
+});
+
+// Functie om de animatie voor de <li> elementen uit te voeren
+function animateDropdownItems() {
+    // Haal een referentie naar het dropdown-menu-element op
+    const dropdownMenu = document.getElementById("dropdown-menu");
+
+    // Haal een lijst van alle <li> elementen in het dropdown-menu
+    const dropdownItems = dropdownMenu.querySelectorAll("li");
+
+    // Begin met het verbergen van alle <li> elementen
+    dropdownItems.forEach((item, index) => {
+        item.style.transition = "none";
+        item.style.transform = "translateY(-100%)";
+        item.style.opacity = 0;
+    });
+
+    // Voer de animatie uit voor elk <li> element
+    dropdownItems.forEach((item, index) => {
+        setTimeout(() => {
+            item.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease-in-out";
+            item.style.transform = "translateY(0)";
+            item.style.opacity = 1;
+        }, index * 100); // Vertraging op basis van de volgorde van de elementen
+    });
+}
+
+// Functie om de omgekeerde animatie uit te voeren
+function reverseAnimateDropdownItems() {
+    // Haal een lijst van alle <li> elementen in het dropdown-menu
+    const dropdownItems = document.querySelectorAll("#dropdown-menu li");
+
+    // Voer de omgekeerde animatie uit voor elk <li> element
+    dropdownItems.forEach((item, index) => {
+        setTimeout(() => {
+            item.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease-in-out";
+            item.style.transform = "translateY(-100%)";
+            item.style.opacity = 0;
+        }, index * 100); // Vertraging op basis van de volgorde van de elementen
+    });
+}
 
 
 
