4946f56d3b5f8b2e312d2029d5c50850f52bf47b
Ismail Ameksa <117262493+DRismaill@users.noreply.github.com>
2023-09-15 18:27:40 +0200

    footer

---
+++ js/curiofoot.js
@@ -0,0 +1,14 @@
+// // Selecteer het pseudo-element voor de link met behulp van ::before
+// const curioLink = document.querySelector('.trd-section-info-footer a::before');
+
+// // Voeg een eventlistener toe voor de "mouseenter" gebeurtenis
+// curioLink.parentElement.addEventListener('mouseenter', () => {
+//     // Verander de breedte van het pseudo-element om de animatie te starten
+//     curioLink.style.width = '100%';
+// });
+
+// // Voeg een eventlistener toe voor de "mouseleave" gebeurtenis om de animatie terug te zetten
+// curioLink.parentElement.addEventListener('mouseleave', () => {
+//     // Verander de breedte van het pseudo-element om de animatie terug te zetten
+//     curioLink.style.width = '0';
+// });
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,25 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
     const dropdownButton = document.getElementById("dropdown-button-id");
     const dropdownMenu = document.getElementById("dropdown-menu");
+    const dropdownOptions = document.querySelectorAll(".dropdown-option");
+
+    // Functie om de animatie voor de <li> elementen uit te voeren
+    function animateDropdownItems() {
+        dropdownOptions.forEach(function (option, index) {
+            option.style.transitionDelay = `${0.1 * index}s`;
+            option.classList.add("active");
+        });
+    }
+
+    // Functie om de omgekeerde animatie uit te voeren
+    function reverseAnimateDropdownItems() {
+        dropdownOptions.forEach(function (option, index) {
+            option.style.transitionDelay = "0s";
+            option.classList.remove("active");
+        });
+    }
 
     // Toon/verberg het dropdown-menu bij het klikken op de button
     dropdownButton.addEventListener("click", function () {
         if (dropdownMenu.style.display === "block") {
+            // Voer de omgekeerde animatie uit voor elk <li> element
+            reverseAnimateDropdownItems();
             dropdownMenu.style.display = "none";
         } else {
             dropdownMenu.style.display = "block";
+            // Voer de animatie uit voor elk <li> element
+            animateDropdownItems();
         }
     });
 
     // Sluit het dropdown-menu als ergens anders wordt geklikt
     document.addEventListener("click", function (event) {
         if (event.target !== dropdownButton && event.target !== dropdownMenu) {
+            // Voer de omgekeerde animatie uit voor elk <li> element
+            reverseAnimateDropdownItems();
             dropdownMenu.style.display = "none";
         }
     });
 
     // Voeg klikgedrag toe aan de dropdown-opties
-    const dropdownOptions = document.querySelectorAll(".dropdown-option");
     dropdownOptions.forEach(function (option) {
         option.addEventListener("click", function () {
             // Verander de tekst van de dropdown-button naar de geselecteerde optie
@@ -29,3 +51,32 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownButton = document.getElementById("dropdown-button-id");
+    const dropdownMenu = document.getElementById("dropdown-menu");
+
+    // Toon/verberg het dropdown-menu bij het klikken op de button
+    dropdownButton.addEventListener("click", function () {
+        if (dropdownMenu.style.maxHeight) {
+            dropdownMenu.style.maxHeight = null; // Terug naar oorspronkelijke hoogte
+        } else {
+            dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + "px"; // Toon het menu met animatie
+        }
+    });
+
+    // Sluit het dropdown-menu als ergens anders wordt geklikt
+    document.addEventListener("click", function (event) {
+        if (event.target !== dropdownButton && event.target !== dropdownMenu) {
+            dropdownMenu.style.maxHeight = null; // Sluit het menu en verwijder de animatie
+        }
+    });
+
+    // Voeg klikgedrag toe aan de dropdown-opties (blijft hetzelfde)
+});
+
+
+
